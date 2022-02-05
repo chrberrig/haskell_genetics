58d2315e7adc86345721724f0beed8c4d1d766f5
Christian Edsberg Møllgaard <cmollgaard@gmail.com>
2022-02-05 10:21:47 +0100

    Initial types

--- app/Main.hs
+++ app/Main.hs
@@ -2,3 +2,14 @@ module Main where
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
+
+
+type RandomFunc = () => Double
+data VariantState = Simple Double RandomFunc
+type State = [VariantState]
+
+-- updateState :: State -> State
+-- normalizeState :: Sate -> State
+
+-- variantUpdateStep :: VariantState -> VariantState
+
