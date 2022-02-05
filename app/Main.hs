77b130410968e2c87c14362cd172b962027a3e1d
Christian Edsberg Møllgaard <cmollgaard@gmail.com>
2022-02-05 10:57:45 +0100

    Initial something

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,29 @@
 module Main where
+import GHC.Event.Windows (CompletionCallback)
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
 
-
 type RandomFunc = () => Double
-data VariantState = Simple Double RandomFunc
+type Proportion = Double
+data VariantState = Simple Proportion RandomFunc
 type State = [VariantState]
 
 -- updateState :: State -> State
 -- normalizeState :: Sate -> State
+-- reportState :: State -> State
 
--- variantUpdateStep :: VariantState -> VariantState
+rand :: () -> Double
+rand() = 1.0
 
+variantUpdateStep :: VariantState -> VariantState
+variantUpdateStep (Simple proportion func) = Simple (proportion * func) func
+
+getProportion :: VariantState -> Double
+getProportion (Simple proportion _) = proportion
+
+getVariant :: () -> VariantState
+getVariant () = Simple 0.5 (rand())
+
+-- normalizeState :: State -> State
+-- normalizeState states = map getProportion states
